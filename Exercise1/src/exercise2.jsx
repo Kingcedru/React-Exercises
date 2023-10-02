fc1d0017e3c4bbe41e8d0950ff14aa55e9fae05e
Kingcedru <byishimocedrick@gmail.com>
2023-10-02 11:53:23 +0200

    deleted on main

--- Exercise1/src/exercise2.jsx
+++
@@ -1,9 +0,0 @@
-
-export default function Buttons(){
-    return(
-    <div className='flex'>
-        <button className="border-2 rounded-lg px-3" id="btn" onClick={()=>alert("clicked")}>Click me</button>
-    </div>
-    )
-}
-
