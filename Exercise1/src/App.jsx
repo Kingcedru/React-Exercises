bc3c20e296295ab2997202f21919c74c8e3f6dd1
Kingcedru <byishimocedrick@gmail.com>
2023-10-02 12:49:49 +0200

    exercise 5

--- Exercise1/src/App.jsx
+++ Exercise1/src/App.jsx
@@ -4,6 +4,7 @@ import Hello from './exercise1'
 import Buttons from './exercise2'
 import ThreeButtons from './exercise3'
 import ClickedTimes from './exercise4'
+import List from './exercise5'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,6 +17,7 @@ function App() {
         <li><a href='#exercise2'>Exercise 2</a></li>
         <li><a href='#exercise3'>Exercise 3</a></li>
         <li><a href='#exercise4'>Exercise 4</a></li>
+        <li><a href='#exercise5'>Exercise 5</a></li>
       </ul>
       </nav>
       <div id='exercise1' className='flex justify-center items-center h-screen'>
@@ -29,6 +31,9 @@ function App() {
       </div>
       <div id='exercise4' className='flex justify-center items-center h-screen'>
         <ClickedTimes/>
+      </div>
+      <div id='exercise5' className='flex justify-center items-center h-screen'>
+        <List/>
       </div>
     </div>
 
