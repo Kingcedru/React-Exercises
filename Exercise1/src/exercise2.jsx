d624454e81252ebd06c213d1696463cbb009eb8c
Kingcedru <byishimocedrick@gmail.com>
2023-10-05 14:37:26 +0200

    exercise 2

---
+++ Exercise1/src/exercise2.jsx
@@ -0,0 +1,9 @@
+
+export default function Buttons(){
+    return(
+    <div className='flex'>
+        <button className="border-2 rounded-lg px-3" id="btn" onClick={()=>alert("clicked")}>Click me</button>
+    </div>
+    )
+}
+
